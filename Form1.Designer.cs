53dae01c8f2d12db06ed4ed72ce004f1fa1877f8
Marcin <marcin.morawiec8@gmail.com>
2024-09-17 10:56:29 +0200

    Create window design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            btnNumber7 = new Button();
+            btnNumber8 = new Button();
+            btnNumber9 = new Button();
+            btnOpAdd = new Button();
+            btnNumber4 = new Button();
+            btnNumber5 = new Button();
+            btnNumber6 = new Button();
+            btnOpSub = new Button();
+            btnNumber1 = new Button();
+            btnNumber2 = new Button();
+            btnNumber3 = new Button();
+            btnOpMul = new Button();
+            btnNumber0 = new Button();
+            btnOpPoint = new Button();
+            btnOpDiv = new Button();
+            btnOpClear = new Button();
+            btnOpEq = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            textBox1.Location = new Point(13, 12);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(575, 43);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnNumber7
+            // 
+            btnNumber7.Location = new Point(12, 65);
+            btnNumber7.Margin = new Padding(4, 5, 4, 5);
+            btnNumber7.Name = "btnNumber7";
+            btnNumber7.Size = new Size(107, 123);
+            btnNumber7.TabIndex = 1;
+            btnNumber7.Text = "7";
+            btnNumber7.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber8
+            // 
+            btnNumber8.Location = new Point(129, 65);
+            btnNumber8.Margin = new Padding(4, 5, 4, 5);
+            btnNumber8.Name = "btnNumber8";
+            btnNumber8.Size = new Size(107, 123);
+            btnNumber8.TabIndex = 2;
+            btnNumber8.Text = "8";
+            btnNumber8.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber9
+            // 
+            btnNumber9.Location = new Point(246, 65);
+            btnNumber9.Margin = new Padding(4, 5, 4, 5);
+            btnNumber9.Name = "btnNumber9";
+            btnNumber9.Size = new Size(107, 123);
+            btnNumber9.TabIndex = 3;
+            btnNumber9.Text = "9";
+            btnNumber9.UseVisualStyleBackColor = true;
+            // 
+            // btnOpAdd
+            // 
+            btnOpAdd.Location = new Point(366, 65);
+            btnOpAdd.Margin = new Padding(4, 5, 4, 5);
+            btnOpAdd.Name = "btnOpAdd";
+            btnOpAdd.Size = new Size(107, 123);
+            btnOpAdd.TabIndex = 4;
+            btnOpAdd.Text = "+";
+            btnOpAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber4
+            // 
+            btnNumber4.Location = new Point(12, 200);
+            btnNumber4.Margin = new Padding(4, 5, 4, 5);
+            btnNumber4.Name = "btnNumber4";
+            btnNumber4.Size = new Size(107, 123);
+            btnNumber4.TabIndex = 5;
+            btnNumber4.Text = "4";
+            btnNumber4.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber5
+            // 
+            btnNumber5.Location = new Point(127, 200);
+            btnNumber5.Margin = new Padding(4, 5, 4, 5);
+            btnNumber5.Name = "btnNumber5";
+            btnNumber5.Size = new Size(107, 123);
+            btnNumber5.TabIndex = 6;
+            btnNumber5.Text = "5";
+            btnNumber5.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber6
+            // 
+            btnNumber6.Location = new Point(246, 200);
+            btnNumber6.Margin = new Padding(4, 5, 4, 5);
+            btnNumber6.Name = "btnNumber6";
+            btnNumber6.Size = new Size(107, 123);
+            btnNumber6.TabIndex = 7;
+            btnNumber6.Text = "6";
+            btnNumber6.UseVisualStyleBackColor = true;
+            // 
+            // btnOpSub
+            // 
+            btnOpSub.Location = new Point(366, 200);
+            btnOpSub.Margin = new Padding(4, 5, 4, 5);
+            btnOpSub.Name = "btnOpSub";
+            btnOpSub.Size = new Size(107, 123);
+            btnOpSub.TabIndex = 8;
+            btnOpSub.Text = "-";
+            btnOpSub.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber1
+            // 
+            btnNumber1.Location = new Point(12, 336);
+            btnNumber1.Margin = new Padding(4, 5, 4, 5);
+            btnNumber1.Name = "btnNumber1";
+            btnNumber1.Size = new Size(107, 123);
+            btnNumber1.TabIndex = 9;
+            btnNumber1.Text = "1";
+            btnNumber1.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber2
+            // 
+            btnNumber2.Location = new Point(129, 336);
+            btnNumber2.Margin = new Padding(4, 5, 4, 5);
+            btnNumber2.Name = "btnNumber2";
+            btnNumber2.Size = new Size(107, 123);
+            btnNumber2.TabIndex = 10;
+            btnNumber2.Text = "2";
+            btnNumber2.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber3
+            // 
+            btnNumber3.Location = new Point(246, 336);
+            btnNumber3.Margin = new Padding(4, 5, 4, 5);
+            btnNumber3.Name = "btnNumber3";
+            btnNumber3.Size = new Size(107, 123);
+            btnNumber3.TabIndex = 11;
+            btnNumber3.Text = "3";
+            btnNumber3.UseVisualStyleBackColor = true;
+            // 
+            // btnOpMul
+            // 
+            btnOpMul.Location = new Point(364, 336);
+            btnOpMul.Margin = new Padding(4, 5, 4, 5);
+            btnOpMul.Name = "btnOpMul";
+            btnOpMul.Size = new Size(107, 123);
+            btnOpMul.TabIndex = 12;
+            btnOpMul.Text = "*";
+            btnOpMul.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber0
+            // 
+            btnNumber0.Location = new Point(12, 471);
+            btnNumber0.Margin = new Padding(4, 5, 4, 5);
+            btnNumber0.Name = "btnNumber0";
+            btnNumber0.Size = new Size(224, 123);
+            btnNumber0.TabIndex = 13;
+            btnNumber0.Text = "0";
+            btnNumber0.UseVisualStyleBackColor = true;
+            // 
+            // btnOpPoint
+            // 
+            btnOpPoint.Location = new Point(246, 471);
+            btnOpPoint.Margin = new Padding(4, 5, 4, 5);
+            btnOpPoint.Name = "btnOpPoint";
+            btnOpPoint.Size = new Size(107, 123);
+            btnOpPoint.TabIndex = 14;
+            btnOpPoint.Text = ".";
+            btnOpPoint.UseVisualStyleBackColor = true;
+            // 
+            // btnOpDiv
+            // 
+            btnOpDiv.Location = new Point(364, 471);
+            btnOpDiv.Margin = new Padding(4, 5, 4, 5);
+            btnOpDiv.Name = "btnOpDiv";
+            btnOpDiv.Size = new Size(107, 123);
+            btnOpDiv.TabIndex = 15;
+            btnOpDiv.Text = "/";
+            btnOpDiv.UseVisualStyleBackColor = true;
+            // 
+            // btnOpClear
+            // 
+            btnOpClear.Location = new Point(481, 65);
+            btnOpClear.Margin = new Padding(4, 5, 4, 5);
+            btnOpClear.Name = "btnOpClear";
+            btnOpClear.Size = new Size(107, 258);
+            btnOpClear.TabIndex = 16;
+            btnOpClear.Text = "C";
+            btnOpClear.UseVisualStyleBackColor = true;
+            // 
+            // btnOpEq
+            // 
+            btnOpEq.Location = new Point(481, 336);
+            btnOpEq.Margin = new Padding(4, 5, 4, 5);
+            btnOpEq.Name = "btnOpEq";
+            btnOpEq.Size = new Size(107, 258);
+            btnOpEq.TabIndex = 17;
+            btnOpEq.Text = "=";
+            btnOpEq.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 603);
+            Controls.Add(btnOpEq);
+            Controls.Add(btnOpClear);
+            Controls.Add(btnOpDiv);
+            Controls.Add(btnOpPoint);
+            Controls.Add(btnNumber0);
+            Controls.Add(btnOpMul);
+            Controls.Add(btnNumber3);
+            Controls.Add(btnNumber2);
+            Controls.Add(btnNumber1);
+            Controls.Add(btnOpSub);
+            Controls.Add(btnNumber6);
+            Controls.Add(btnNumber5);
+            Controls.Add(btnNumber4);
+            Controls.Add(btnOpAdd);
+            Controls.Add(btnNumber9);
+            Controls.Add(btnNumber8);
+            Controls.Add(btnNumber7);
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            Margin = new Padding(4, 5, 4, 5);
+            MaximumSize = new Size(620, 650);
+            MinimumSize = new Size(620, 650);
+            Name = "Form1";
+            Text = "Kalkulator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnNumber7;
+        private System.Windows.Forms.Button btnNumber8;
+        private System.Windows.Forms.Button btnNumber9;
+        private System.Windows.Forms.Button btnOpAdd;
+        private System.Windows.Forms.Button btnNumber4;
+        private System.Windows.Forms.Button btnNumber5;
+        private System.Windows.Forms.Button btnNumber6;
+        private System.Windows.Forms.Button btnOpSub;
+        private System.Windows.Forms.Button btnNumber1;
+        private System.Windows.Forms.Button btnNumber2;
+        private System.Windows.Forms.Button btnNumber3;
+        private System.Windows.Forms.Button btnOpMul;
+        private System.Windows.Forms.Button btnNumber0;
+        private System.Windows.Forms.Button btnOpPoint;
+        private System.Windows.Forms.Button btnOpDiv;
+        private System.Windows.Forms.Button btnOpClear;
+        private System.Windows.Forms.Button btnOpEq;
     }
 }
 
